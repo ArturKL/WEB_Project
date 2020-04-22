e6999b2d66277ce307496109ef2942ab966efb94
ArturKL <arturkalimullin10@gmail.com>
2020-04-22 16:53:44 +0300

    Reviews edit and delete. Reviews resourses

--- data/forms.py
+++ data/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import *
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 from wtforms.fields.html5 import EmailField, IntegerField
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField('E-mail', validators=[DataRequired()])
+    email = EmailField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторить пароль', validators=[DataRequired()])
     name = StringField('Имя', validators=[DataRequired()])
@@ -19,3 +19,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить')
     submit = SubmitField('Войти')
+
+
+class ReviewForm(FlaskForm):
+    rating = SelectField('Оценка', choices=[('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5)][::-1], validators=[
+        DataRequired()])
+    content = TextAreaField('Отзыв', validators=[Length(max=500, message='Максимальный размер отзыва - 500 символов')])
+    submit = SubmitField('Оставить отзыв')
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data.users import User
 from data.reviews import Review
 from data.forms import *
 import datetime
+from reviews_resourses import ReviewsResource, ReviewsListResource
 
 
 app = Flask(__name__)
@@ -30,7 +31,7 @@ def main_page():
 @app.route('/feedback')
 def feedback():
     session = db_session.create_session()
-    reviews = session.query(Review).all()
+    reviews = session.query(Review).all()[::-1]
     return render_template('feedback.html', title='Отзывы', reviews=reviews)
 
 
@@ -77,6 +78,56 @@ def logout():
     return redirect('/feedback')
 
 
+@app.route('/review_form', methods=['GET', 'POST'])
+def review_form():
+    form = ReviewForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        review = Review()
+        review.user_id = current_user.id
+        review.user_name = current_user.name
+        review.user_surname = current_user.surname
+        review.rating = form.rating.data
+        review.content = form.content.data
+        review.date = datetime.date.today()
+        session.add(review)
+        session.commit()
+        return redirect('/feedback')
+    else:
+        return render_template('review_form.html', form=form)
+
+
+@app.route('/review/edit/<int:id>', methods=['GET', 'POST'])
+def edit_review(id):
+    form = ReviewForm()
+    session = db_session.create_session()
+    review = session.query(Review).filter(Review.id == id, Review.user_id == current_user.id).first()
+    if not review:
+        return render_template('404.html', title='Ошибка 404')
+    if form.validate_on_submit():
+        review.rating = form.rating.data
+        review.content = form.content.data
+        session.commit()
+        return redirect('/feedback')
+    else:
+        form.rating.data = review.rating
+        form.content.data = review.content
+        return render_template('review_form.html', title='Изменить отзыв', form=form)
+
+
+@app.route('/review/delete/<int:id>', methods=['GET', 'POST'])
+def delete_review(id):
+    session = db_session.create_session()
+    review = session.query(Review).filter(Review.id == id,
+                                         (Review.user_id == current_user.id) | (current_user.id == 1)).first()
+    if review:
+        session.delete(review)
+        session.commit()
+        return redirect('/feedback')
+    else:
+        return render_template('404.html', title='Ошибка 404')
+
+
 def feel_db():
     session = db_session.create_session()
     for i in range(1, 6):
@@ -95,4 +146,6 @@ def feel_db():
 if __name__ == '__main__':
     app.template_folder = 'template'
     db_session.global_init('db/main_db.sqlite')
+    api.add_resource(ReviewsResource, '/api/review/<int:id>')
+    api.add_resource(ReviewsListResource, '/api/reviews/<int:rating>')
     app.run(port=5000, host='127.0.0.1')
